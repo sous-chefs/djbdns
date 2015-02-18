7dbbdc4b0fdb6385c56f54e18164d7a8ffcc2cd0
Sean OMeara <sean@chef.io>
2015-02-18 11:55:06 -0500

    opscode2chef

--- recipes/internal_server.rb
+++ recipes/internal_server.rb
@@ -1,9 +1,9 @@
 #
-# Author:: Joshua Timberman (<joshua@opscode.com>)
+# Author:: Joshua Timberman (<joshua@chef.io>)
 # Cookbook Name:: djbdns
 # Recipe:: internal_server
 #
-# Copyright 2009, Opscode, Inc
+# Copyright 2009, Chef Software, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
