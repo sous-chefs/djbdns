0d86d3e558dc921b253ca4edd75ac16e98bd6eaf
Tim Smith <tsmith@chef.io>
2017-01-17 22:30:11 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -24,4 +24,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/djbdns'
 issues_url 'https://github.com/chef-cookbooks/djbdns/issues'
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
